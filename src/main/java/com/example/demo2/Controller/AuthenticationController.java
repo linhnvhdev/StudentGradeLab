56403eec514047edda36f1cde882e7d9e5df15cb
loidinhcap <loidinhcap@loidinhcap-Lenovo-V15-IIL>
2023-03-30 01:57:59 +0700

    login, push user to session

--- src/main/java/com/example/demo2/Controller/AuthenticationController.java
+++ src/main/java/com/example/demo2/Controller/AuthenticationController.java
@@ -6,13 +6,15 @@ package com.example.demo2.Controller;
 
 import com.example.demo2.Model.User;
 import com.example.demo2.dao.UserRepository;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -29,17 +31,23 @@ public class AuthenticationController {
         return "login";
     }
     
-    @PostMapping(value = "login")
-    public String access(@ModelAttribute("user") User user, Model model){
+    @PostMapping(value = "/login")
+    public String access(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, Model model, HttpServletRequest request){
+        User user = new User();
+        user.setCode(username);
+        user.setPassword(password);
         User userDb = userRepository.findById(user.getCode()).get();
-//        if(userDb.getPassword().equals(user.getPassword())){
-//            
-//            return "home";
-//        }else{
-//            model.addAttribute("error", "Please check your username and password!");
-//            return "login";
-//        }
-        model.addAttribute("user", userDb);
-        return "home";
+        if(userDb.getPassword().equals(user.getPassword())){
+            request.getSession().setAttribute("user", user);
+            return "home";
+        }else{
+            model.addAttribute("error", "Please check your username and password!");
+            return "login";
+        }
+    }
+    
+    @GetMapping("/logout")
+    public String logout(){
+        return "login";
     }
 }
