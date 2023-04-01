1c1ca45941aecad4c2ce193a215ea341a50ad175
quang <quang@quang>
2023-04-01 16:57:20 +0700

    admin grade view filters

--- src/main/java/com/example/demo2/Controller/GradeAdminController.java
+++ src/main/java/com/example/demo2/Controller/GradeAdminController.java
@@ -5,21 +5,34 @@
 package com.example.demo2.Controller;
 
 import com.example.demo2.Model.Grade;
+import com.example.demo2.Model.Major;
 import com.example.demo2.Model.Student;
 import com.example.demo2.Model.StudentAverageGrade;
 import com.example.demo2.Model.StudentSemesterCourse;
+import com.example.demo2.Model.User;
 import com.example.demo2.Repository.GradeRepository;
+import com.example.demo2.Repository.MajorRepository;
+import com.example.demo2.Repository.SemesterRepository;
 import com.example.demo2.Repository.StudentRepository;
 import com.example.demo2.Service.GradeCalculatingService;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -31,25 +44,84 @@ public class GradeAdminController {
     @Autowired
     private GradeRepository gradeRepository;
     @Autowired
+    private MajorRepository majorRepository;
+    @Autowired
+    private SemesterRepository semesterRepository;
+    @Autowired
     private GradeCalculatingService service;
 
     @RequestMapping(value = "/admin/admin-grades-list", method = RequestMethod.GET)
-    public String getGradesList(Model model) {
+    public String getGradesList(
+            @RequestParam(name = "s_sort", defaultValue = "0") int s_sort,
+            @RequestParam(name = "g_sort", defaultValue = "none") String g_sort,
+            @RequestParam(name = "m_sort", defaultValue = "0") int m_sort,
+            Model model) {
         Map<StudentSemesterCourse, StudentAverageGrade> gradeMap = new HashMap<>();
         List<Grade> gradeList = gradeRepository.findAll();
         for (int i = 0; i < gradeList.size(); i++) {
             StudentSemesterCourse key
                     = new StudentSemesterCourse(gradeList.get(i).getStudent(),
                             gradeList.get(i).getSemester(), gradeList.get(i).getCourse());
-            if (!gradeMap.containsKey(key)) {
-                float average = service.
-                        AverageGradeStudentSemester(gradeList.get(i).getStudent_id(), gradeList.get(i).getSemester_id());
+            if (gradeMap.containsKey(key)) {
+                continue;
+            }
+            float average = service.
+                    AverageGradeStudentSemester(gradeList.get(i).getStudent_id(), gradeList.get(i).getSemester_id());
+            if (s_sort == 0 && m_sort == 0) {
                 gradeMap.put(key,
                         new StudentAverageGrade(gradeList.get(i).getStudent(),
                                 gradeList.get(i).getSemester(), average));
+            } else if (s_sort != 0 && m_sort == 0) {
+                if (gradeList.get(i).getSemester().getId() == s_sort) {
+                    gradeMap.put(key,
+                            new StudentAverageGrade(gradeList.get(i).getStudent(),
+                                    gradeList.get(i).getSemester(), average));
+                }
+            } else if (s_sort == 0 && m_sort != 0) {
+                if (gradeList.get(i).getStudent().getCurriculum().getDept().getMajor().getId() == m_sort) {
+                    gradeMap.put(key,
+                            new StudentAverageGrade(gradeList.get(i).getStudent(),
+                                    gradeList.get(i).getSemester(), average));
+                }
+            } else {
+                if (gradeList.get(i).getStudent().getCurriculum().getDept().getMajor().getId() == m_sort
+                        && gradeList.get(i).getSemester().getId() == s_sort) {
+                    gradeMap.put(key,
+                            new StudentAverageGrade(gradeList.get(i).getStudent(),
+                                    gradeList.get(i).getSemester(), average));
+                }
             }
         }
-        model.addAttribute("list", gradeMap);
+
+        if (g_sort.equalsIgnoreCase("none")) {
+            model.addAttribute("list", gradeMap);
+        } else {
+            List<Map.Entry<StudentSemesterCourse, StudentAverageGrade>> sortedList = new ArrayList<>(gradeMap.entrySet());
+            Collections.sort(sortedList, new Comparator<Map.Entry<StudentSemesterCourse, StudentAverageGrade>>() {
+                @Override
+                public int compare(Map.Entry<StudentSemesterCourse, StudentAverageGrade> o1, Map.Entry<StudentSemesterCourse, StudentAverageGrade> o2) {
+                    int compareResult = Float.compare(o2.getValue().getAverageGrade(), o1.getValue().getAverageGrade());
+                    if (g_sort.equalsIgnoreCase("asc")) {
+                        return -compareResult;
+                    } else if (g_sort.equalsIgnoreCase("desc")) {
+                        return compareResult;
+                    }
+                    return 0;
+                }
+            });
+            Map<StudentSemesterCourse, StudentAverageGrade> sortedMap = new LinkedHashMap<>();
+            for (Map.Entry<StudentSemesterCourse, StudentAverageGrade> entry : sortedList) {
+                sortedMap.put(entry.getKey(), entry.getValue());
+            }
+            model.addAttribute("list", sortedMap);
+        }
+
+        model.addAttribute("s_sort", s_sort);
+        model.addAttribute("g_sort", g_sort);
+        model.addAttribute("m_sort", m_sort);
+        model.addAttribute("semesters", semesterRepository.findAll());
+        model.addAttribute("majors", majorRepository.findAll());
+
         return "admin-grades-list";
     }
 
--- src/main/webapp/WEB-INF/jsp/admin-grades-list.jsp
+++ src/main/webapp/WEB-INF/jsp/admin-grades-list.jsp
@@ -18,18 +18,57 @@
             <title>JSP Page</title>
         </head>
         <body>
-            <table border="5px">
-                <tr>
-                    <td>Student</td>
-                    <td>Student Code</td>
-                    <td>Major</td>
-                    <td>Semester</td>
-                    <td>Average</td>
-                </tr>
+            <form action="/admin/admin-grades-list" method="get">
+                Semester : <select name="s_sort">
+                    <option value="0"
+                            <c:if test="${0 == s_sort}">selected</c:if>
+                                >None                        
+                            </option>
+                    <c:forEach items="${semesters}" var="sem">
+                        <option value="${sem.id}"
+                                <c:if test="${sem.id == s_sort}">selected</c:if>
+                                    >
+                                ${sem.name}</option>
+                        </c:forEach>
+                </select>
+                Major : <select name="m_sort">
+                    <option value="0"
+                            <c:if test="${0 == m_sort}">selected</c:if>
+                                >None                        
+                            </option>
+                    <c:forEach items="${majors}" var="sem">
+                        <option value="${sem.id}"
+                                <c:if test="${sem.id == m_sort}">selected</c:if>
+                                    >
+                                ${sem.name}</option>
+                        </c:forEach>
+                </select>
+                Sort: <select name="g_sort">
+                    <option value="none"
+                            <c:if test="${g_sort == 'none'}">selected</c:if>
+                                >None                        
+                            </option>
+                            <option value="asc"
+                            <c:if test="${g_sort == 'asc'}">selected</c:if>
+                                >Grade ascending</option>
+                            <option value="desc"
+                            <c:if test="${g_sort == 'desc'}">selected</c:if> 
+                                >Grade descending</option>
+                    </select>
+                    <input name="submit" value="Filter" type="submit" />
+                </form>
+                <table border="5px">
+                    <tr>
+                        <td>Student</td>
+                        <td>Student Code</td>
+                        <td>Major</td>
+                        <td>Semester</td>
+                        <td>Average</td>
+                    </tr>
                 <c:forEach items="${list}" var="l">
                     <tr>
                         <td>${l.value.student.name}</td>
-                        <td>${l.value.student.user.password}</td>
+                        <td>${l.value.student.user.code}</td>
                         <td>${l.value.student.curriculum.dept.major.name}</td>
                         <td>${l.value.semester.name}</td>
                         <td>${l.value.averageGrade}</td>
